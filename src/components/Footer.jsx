ebac5b0866abbbe1815c22f088e957eb8470d320
Naman Gupta <naman.mw4@gmail.com>
2024-06-05 15:35:29 +0530

    Insert Git Project Link

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -6,25 +6,25 @@ import { FaGithub } from "react-icons/fa";
 const Footer = () => {
   const currentYear = new Date().getFullYear();
   return (
-    <section className="bg-gray-200">
+    <section className="bg-gray-200 mt-4">
       <footer className="container mx-auto px-5 py-10 gap-y-10 gap-x-5 md:pt-20 lg:gap-x-10">
         <div className="md:text-base">
           <h1 className="font-courierprime text-2xl lg:text-3xl md:text-3xl xl:text-3xl text-blacktext text-center">
-            My Bookshelf
+            Bookshelf
           </h1>
           <p className="text-blacktext text-xs text-center mt-4 mb-8">
             Read Anywhere, Anytime: E-Books at Your Fingertips.
           </p>
-          <ul className="flex justify-center items-center mt-5 mb-16 space-x-4 text-blacktext">
-            <div className="flex items-center gap-x-4 rounded-full px-4 py-2 bg-gray-100 hover:bg-gray-300 duration-300">
-              <li>
-                <a href="/">
+          <a href="https://github.com/namanx19/Marquee-Equity-MERN-Assessment.git" target="_blank" rel="noreferrer">
+            <ul className="flex justify-center items-center mt-5 mb-16 space-x-4 text-blacktext">
+              <div className="flex items-center gap-x-4 rounded-full px-4 py-2 bg-gray-100 hover:bg-gray-300 duration-300">
+                <li>
                   <FaGithub className="w-6 h-auto" />
-                </a>
-              </li>
-              <h1>My Bookshelf</h1>
-            </div>
-          </ul>
+                </li>
+                <h1>Bookshelf Codebase</h1>
+              </div>
+            </ul>
+          </a>
         </div>
         <div className="flex flex-col items-center space-y-4 col-span-12">
           <div className="bg-dark-soft bg-gray-300 p-3 rounded-full">
