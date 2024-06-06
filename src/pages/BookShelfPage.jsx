d22445e5f2a85359acfd4be18acd953f986580e0
Naman Gupta <naman.mw4@gmail.com>
2024-06-06 13:47:23 +0530

    Fix Grid Spacing on BookShelfPage

--- src/pages/BookShelfPage.jsx
+++ src/pages/BookShelfPage.jsx
@@ -1,11 +1,21 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import BookCard from "../components/BookCard";
 import Lottie from "react-lottie";
 import animationData from "../assets/BookShelfPageOops.json";
 
 const BookshelfPage = () => {
-  const books = JSON.parse(localStorage.getItem("bookshelf")) || [];
+  const [books, setBooks] = useState(JSON.parse(localStorage.getItem("bookshelf")) || []);
   const isLocalStorageEmpty = books.length === 0;
+
+  useEffect(() => {
+    setBooks(JSON.parse(localStorage.getItem("bookshelf")) || []);
+  }, []);
+
+  const removeFromBookshelf = (book) => {
+    const updatedBooks = books.filter((b) => b.key !== book.key);
+    setBooks(updatedBooks);
+    localStorage.setItem("bookshelf", JSON.stringify(updatedBooks));
+  };
 
   const defaultOptions = {
     loop: true,
@@ -30,10 +40,10 @@ const BookshelfPage = () => {
         </div>
       )}
 
-      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 justify-center">
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 justify-center px-60 py-2">
         {books.map((book, index) => (
           <div key={index} className="flex justify-center">
-            <BookCard book={book} showAddButton={false} />
+            <BookCard book={book} showAddButton={false} showRemoveButton={true} removeFromBookshelf={removeFromBookshelf} />
           </div>
         ))}
       </div>
